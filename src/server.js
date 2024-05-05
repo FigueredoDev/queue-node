d95986db9b75678d2c3821a6f7f79cc074c06288
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-05-05 19:06:51 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.APPLICATION_PORT || 3000
+
+app.get('/users', (req, res) => {
+    return res.send("hello world")
+})
+
+app.listen(port, () => console.log(`Server is running on port ${port}!`))
