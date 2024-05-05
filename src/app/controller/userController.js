d67f9796153ec16989c4846acdf8a39e52319632
JhonataFigueredo <jhowfigueredo2@gmail.com>
2024-05-05 20:14:36 -0300

    feat: add send user email

--- src/app/controller/userController.js
+++ src/app/controller/userController.js
@@ -1,6 +1,20 @@
+import Mail from "../lib/Mail.js";
+
 class UserController {
   async create(request, response) {
-    return response.json({ message: "create user" });
+    const { name, email } = request.body;
+
+    const user = { name, email };
+
+    // TODO: send a confirmation email to the user
+    await Mail.sendMail({
+      from: "Queue test <queue@example.com>",
+      to: `${user.name} <${user.email}>`,
+      subject: "Welcome",
+      html: `Welcome, ${user.name}!`,
+    });
+
+    return response.json(user);
   }
 }
 
